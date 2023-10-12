62e2cb2046171a1cbdd48b39eaa32941f03e4343
Bukakeenlacaradenami99 <cristianrmn93@gmail.com>
2023-10-12 11:15:55 +0200

    la otra parte

---
+++ Uso_extra1.java
@@ -0,0 +1,13 @@
+
+public class Uso_extra1 {
+
+	public static void main(String[] args) {
+		
+		Extra1 persona1=new Extra1("Carlos", 2, 5, 1994);
+		persona1.setNombre();
+		persona1.setFecha();
+				    
+
+	}
+
+}
